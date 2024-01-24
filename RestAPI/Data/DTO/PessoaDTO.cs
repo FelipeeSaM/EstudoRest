df44949cc304d35dd5ff5b6996d53c13c0813ab1
eu mesmo <luiz_sena@hotmail.com>
2024-01-24 19:21:48 -0300

    Remove: Json Serialization

--- RestAPI/Data/DTO/PessoaDTO.cs
+++ RestAPI/Data/DTO/PessoaDTO.cs
@@ -1,20 +1,12 @@
 ﻿using RestAPI.Model.Base;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace RestAPI.Data.DTO {
     public class PessoaDTO {
-
-        //JsonPropertyName muda o nome da propriedade para ser exposta no endpoint.
-        //JsonIgnore faz com que a propriedade não seja mostrada no endpoint.
-
         public int Id { get; set; }
-
-        [JsonPropertyName("PrimeiroNome")]
         public string PrimeiroNome { get; set; }
         public string UltimoNome { get; set; }
         public string Endereco { get; set; }
-        //[JsonIgnore]
         public string Genero { get; set; }
     }
 }
